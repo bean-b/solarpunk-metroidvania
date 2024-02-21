b60ca3cd5e7fd1d9414e9ef631f89ab4fadf422f
reddsaut <osauter@macalester.edu>
2024-02-21 13:32:08 -0600

    attribution

--- Assets/MovingPlatform.cs
+++ Assets/MovingPlatform.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
-
+//attribution: https://youtu.be/hH0OYz7YtKk?feature=shared
 public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private Transform[] _waypoints;
