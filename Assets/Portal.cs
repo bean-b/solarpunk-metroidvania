9f1128f0447c94aaf620c07de66d852003ee437f
reddsaut <osauter@macalester.edu>
2024-02-22 11:14:34 -0600

    thinking with portals

--- Assets/Portal.cs
+++ Assets/Portal.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField] private Portal p2;
     private Vector2 p2p;
+    public float cooldown;
     
     // Start is called before the first frame update
     void Start()
@@ -16,13 +18,19 @@ public class Portal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        }
     }
-    private void OnTriggerEnter(Collider other) {
+    public void Cooldown() {
+        cooldown = 0.5f;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
         print("beep)");
         var playerHandler = other.GetComponent<PlayerHandler>();
-        if(playerHandler != null) {
+        if(cooldown <=0 && playerHandler != null) {
             playerHandler.forceMovement(p2p);
+            p2.Cooldown();
         }
     }
 
